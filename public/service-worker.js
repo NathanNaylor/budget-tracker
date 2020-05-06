98ce12e06c227dc05a72ba4f16da5168d43166aa
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-05-05 20:41:02 -0700

    caching important external libraries

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,6 +13,8 @@ const FILES_TO_CACHE = [
     "/db.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
   ];
   
   const CACHE_NAME = "static-cache-v1";
